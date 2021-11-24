85abcf1f2b4d98955d4cc43e7e26a5294a5211f0
robvk <r.vankruijsdijk@gmail.com>
2021-11-24 17:13:33 +0100

    Check if destructuring is what breaks the PORT

--- server/src/index.js
+++ server/src/index.js
@@ -7,13 +7,13 @@ import { logInfo, logError } from "./util/logging.js";
 dotenv.config();
 
 // The environment should set the port
-const { PORT } = process.env;
+const port = process.env.PORT;
 
-if (PORT == null) {
+if (port == null) {
   // If this fails, make sure you have created a `.env` file in the right place with the PORT set
   logError(new Error("Cannot find a PORT number, did you create a .env file?"));
 }
 
-app.listen(PORT, () => {
-  logInfo(`Server started on port ${PORT}`);
+app.listen(port, () => {
+  logInfo(`Server started on port ${port}`);
 });
