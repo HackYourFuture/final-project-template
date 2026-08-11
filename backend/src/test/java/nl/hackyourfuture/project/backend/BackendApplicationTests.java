c56e800d8ef798223584620998abe11b7688b9d2
stasel <2033301+stasel@users.noreply.github.com>
2026-08-12 00:01:15 +0200

    Fixed tests

--- backend/src/test/java/nl/hackyourfuture/project/backend/BackendApplicationTests.java
+++ backend/src/test/java/nl/hackyourfuture/project/backend/BackendApplicationTests.java
@@ -1,13 +1,17 @@
 package nl.hackyourfuture.project.backend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
